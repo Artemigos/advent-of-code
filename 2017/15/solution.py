6d607d70a8106ab3812e1f7f817d04b73333a126
Łukasz Klimek <klinn91@gmail.com>
2018-10-13 22:48:19 +0200

    added solution for assignment 15

---
+++ 2017/15/solution.py
@@ -0,0 +1,47 @@
+data_a = 783
+data_b = 325
+
+mul_a = 16807
+mul_b = 48271
+mod = 2147483647
+criteria_a = 4
+criteria_b = 8
+
+repetitions_1 = 40000000
+repetitions_2 = 5000000
+cmp_mask = 256*256-1
+
+def create_gen(init, mul, mod, criteria=1):
+    prev = init
+    while True:
+        prev = (prev*mul)%mod
+        if not prev%criteria == 0:
+            continue
+        yield prev
+
+# part 1
+gen_a = create_gen(data_a, mul_a, mod)
+gen_b = create_gen(data_b, mul_b, mod)
+
+matches = 0
+for i in range(repetitions_1):
+    a = next(gen_a)
+    b = next(gen_b)
+    if (a & cmp_mask) == (b & cmp_mask):
+        matches += 1
+
+print(matches)
+
+# part 2
+gen_a = create_gen(data_a, mul_a, mod, criteria_a)
+gen_b = create_gen(data_b, mul_b, mod, criteria_b)
+
+matches = 0
+
+for i in range(repetitions_2):
+    a = next(gen_a)
+    b = next(gen_b)
+    if (a & cmp_mask) == (b & cmp_mask):
+        matches += 1
+
+print(matches)
