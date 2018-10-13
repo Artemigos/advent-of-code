b35bb0172e218df288d46a0396ba5c48072f7ffe
Łukasz Klimek <klinn91@gmail.com>
2018-10-13 16:15:49 +0200

    fixed solution for assignment 10 part 1

--- 2017/10/solution.py
+++ 2017/10/solution.py
@@ -2,34 +2,28 @@ import itertools
 
 data = [192,69,168,160,78,1,166,28,0,83,198,2,254,255,41,12]
 string = list(range(256))
-print(string)
 
 skip = 0
 pos = 0
 
-def roller():
-    i = 0
-    while True:
-        yield string[i]
-        i += 1
-        if i >= 256:
-            i = 0
-
-string_gen = roller()
+def inc_pos(amount):
+    global pos
+    pos += amount
+    while pos >= len(string):
+        pos -= len(string)
 
 for l in data:
-    elements = list(itertools.islice(string_gen, l))
+    start_pos = pos
+    elements = list()
+    for i in range(l):
+        elements.append(string[pos])
+        inc_pos(1)
     elements.reverse()
+    pos = start_pos
     for i in range(l):
         string[pos] = elements[i]
-        pos += 1
-        if pos >= 256:
-            pos = 0
-    pos += skip
+        inc_pos(1)
+    inc_pos(skip)
     skip += 1
-    while pos >= 256:
-        pos -= 256
-    print(string)
 
-# TODO: result wasn't accepted
 print(string[0] * string[1])
