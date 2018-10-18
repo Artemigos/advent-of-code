ef159070d67cfe1e0b46bca43b62a768fa4be600
Łukasz Klimek <klinn91@gmail.com>
2018-10-18 18:05:11 +0200

    added solutions 2016/06 and 2016/07

---
+++ 2016/06/solution.py
@@ -0,0 +1,30 @@
+import common
+
+lines = common.read_file('2016/06/data.txt').splitlines()
+line_len = len(lines[0])
+
+collectors = []
+for i in range(line_len):
+    collectors.append(dict())
+
+for l in lines:
+    for i in range(line_len):
+        coll = collectors[i]
+        if l[i] in coll.keys():
+            coll[l[i]] += 1
+        else:
+            coll[l[i]] = 1
+
+# part 1
+for i in range(line_len):
+    mk, _ = max(collectors[i].items(), key=lambda x: x[1])
+    print(mk, end='')
+
+print()
+
+# part 2
+for i in range(line_len):
+    mk, _ = min(collectors[i].items(), key=lambda x: x[1])
+    print(mk, end='')
+
+print()
---
+++ 2016/07/solution.py
@@ -0,0 +1,63 @@
+import itertools
+import operator
+import common
+
+lines = common.read_file('2016/07/data.txt').splitlines()
+# lines = [
+#     'abba[mnop]qrst',
+#     'abcd[bddb]xyyx',
+#     'aaaa[qwer]tyui',
+#     'ioxxoj[asdfgh]zxcvbn'
+# ]
+
+support_tls = 0
+support_ssl = 0
+for l in lines:
+    segments = list(common.flatmap(lambda x: x.split('['), l.split(']')))
+
+    # part 1
+    found_outside = False
+    found_inside = False
+    for i, s in enumerate(segments):
+        if i%2 == 0: # outside
+            if found_outside:
+                continue
+            else:
+                for j in range(0, len(s)-3):
+                    if s[j] != s[j+1] and s[j] == s[j+3] and s[j+1] == s[j+2]:
+                        found_outside = True
+                        break
+        else: # inside
+            for j in range(0, len(s)-3):
+                if s[j] != s[j+1] and s[j] == s[j+3] and s[j+1] == s[j+2]:
+                    found_inside = True
+                    break
+
+        if found_inside:
+            break
+        
+    if found_outside and not found_inside:
+        support_tls += 1
+
+    # part 2
+    abas = []
+    for i in range(0, len(segments), 2): # outsite
+        s = segments[i]
+        for j in range(0, len(s)-2):
+            if s[j] == s[j+2] and s[j] != s[j+1]:
+                abas.append(s[j:j+3])
+
+    found_bab = False
+    babs = list(map(lambda x: x[1]+x[0]+x[1], abas))
+    for i in range(1, len(segments), 2): # inside
+        s = segments[i]
+        for j in range(0, len(s)-2):
+            if s[j:j+3] in babs:
+                found_bab = True
+                break
+        if found_bab:
+            support_ssl += 1
+            break
+
+print(support_tls)
+print(support_ssl)
