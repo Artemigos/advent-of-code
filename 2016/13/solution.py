1e55d43f8d45688295d9c155faae820b1ff251cc
Łukasz Klimek <klinn91@gmail.com>
2018-10-19 00:37:14 +0200

    added solutions for 2016/13 and 2016/14

---
+++ 2016/13/solution.py
@@ -0,0 +1,55 @@
+import queue
+
+data = 1350
+
+def can_step(x, y):
+    num = x*x + 3*x + 2*x*y + y + y*y + data
+    bin_num = bin(num)
+    cnt = bin_num.count('1')
+    steppable = cnt%2 == 0
+    return steppable
+
+# part 1
+moves = queue.deque([(0, (1, 1))])
+seen_positions = set()
+
+while len(moves) > 0:
+    depth, move = moves.popleft()
+    if move == (31, 39):
+        print('found path at depth', depth)
+        break
+    if move in seen_positions:
+        continue
+    seen_positions.add(move)
+    x, y = move
+    if x < 0 or y < 0:
+        continue
+    if not can_step(x, y):
+        continue
+    moves.append((depth+1, (x+1, y)))
+    moves.append((depth+1, (x-1, y)))
+    moves.append((depth+1, (x, y+1)))
+    moves.append((depth+1, (x, y-1)))
+
+# part 2
+moves = queue.deque([(0, (1, 1))])
+seen_positions = set()
+
+while len(moves) > 0:
+    depth, move = moves.popleft()
+    if depth >= 51:
+        break
+    x, y = move
+    if x < 0 or y < 0:
+        continue
+    if not can_step(x, y):
+        continue
+    if move in seen_positions:
+        continue
+    seen_positions.add(move)
+    moves.append((depth+1, (x+1, y)))
+    moves.append((depth+1, (x-1, y)))
+    moves.append((depth+1, (x, y+1)))
+    moves.append((depth+1, (x, y-1)))
+
+print(len(seen_positions))
---
+++ 2016/14/solution.py
@@ -0,0 +1,72 @@
+import hashlib
+import queue
+import common
+
+data = b'zpqevtbw'
+# data = b'abc'
+base_hash = hashlib.md5(data)
+
+# part 1
+threes = list()
+
+keys_found = 0
+i = -1
+while True:
+    i += 1
+    cp_hash = base_hash.copy()
+    cp_hash.update(bytes(str(i), 'utf8'))
+    hsh = cp_hash.hexdigest()
+
+    charas = queue.deque(['', ''], maxlen=2)
+    charas5 = queue.deque(['', '', '', ''], maxlen=4)
+    three_found = False
+    for c in hsh:
+        if c == charas[0] and c == charas[1] and not three_found:
+            threes.append((i, c))
+            three_found = True
+        if c == charas5[0] and c == charas5[1] and c == charas5[2] and c == charas5[3]:
+            finished_threes = list(filter(lambda x: x[1] == c and i-x[0] <= 1000 and x[0] != i, threes))
+            for f3 in finished_threes:
+                threes.remove(f3)
+                keys_found += 1
+                common.print_and_return(keys_found)
+                if keys_found == 64:
+                    print('64th key at', f3[0])
+                    break
+        charas.append(c)
+        charas5.append(c)
+    if keys_found == 64:
+        break
+
+# part 2
+threes = list()
+
+keys_found = 0
+i = -1
+while True:
+    i += 1
+    cp_hash = base_hash.copy()
+    cp_hash.update(bytes(str(i), 'utf8'))
+    hsh = cp_hash.hexdigest()
+    for _ in range(2016):
+        im_sad = hashlib.md5(bytes(hsh, 'utf8'))
+        hsh = im_sad.hexdigest()
+
+    charas = queue.deque(['', ''], maxlen=2)
+    charas5 = queue.deque(['', '', '', ''], maxlen=4)
+    three_found = False
+    for c in hsh:
+        if c == charas[0] and c == charas[1] and not three_found:
+            threes.append((i, c))
+            three_found = True
+        if c == charas5[0] and c == charas5[1] and c == charas5[2] and c == charas5[3]:
+            finished_threes = list(filter(lambda x: x[1] == c and i-x[0] <= 1000 and x[0] != i, threes))
+            for f3 in finished_threes:
+                threes.remove(f3)
+                keys_found += 1
+                common.print_and_return(keys_found)
+                if keys_found == 64:
+                    print('64th key at', f3[0])
+                    exit(0)
+        charas.append(c)
+        charas5.append(c)
