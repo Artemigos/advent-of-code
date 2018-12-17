b5925e41bb352280e82cdb2adf7de87cb7bab202
Łukasz Klimek <klinn91@gmail.com>
2018-12-17 13:55:56 +0100

    fixed part 1 of 2018/15, started preparing part 2

--- 2018/15/solution.py
+++ 2018/15/solution.py
@@ -4,14 +4,14 @@ import itertools
 import queue
 
 class Unit:
-    def __init__(self, unit_type: str):
+    def __init__(self, unit_type: str, power=3):
         self.unit_type = unit_type
         self.hp = 200
-        self.attack = 3
+        self.attack = power
 
 class Elf(Unit):
-    def __init__(self):
-        super().__init__('E')
+    def __init__(self, power):
+        super().__init__('E', power)
 
 class Goblin(Unit):
     def __init__(self):
@@ -36,9 +36,9 @@ class WorldMap:
         return self.board[y][x]
 
 class GameState:
-    def __init__(self, lines):
+    def __init__(self, lines, elf_power):
         self.map = WorldMap(lines)
-        self.elves = self._read_elves(self.map)
+        self.elves = self._read_elves(self.map, elf_power)
         self.goblins = self._read_goblins(self.map)
 
     def can_walk_at(self, x, y):
@@ -104,13 +104,13 @@ class GameState:
             self.goblins[pos] = unit
 
     @staticmethod
-    def _read_elves(m: WorldMap):
+    def _read_elves(m: WorldMap, elf_power):
         result = {}
         for y in range(m.h):
             for x in range(m.w):
                 c = m.lines[y][x]
                 if c == 'E':
-                    result[y, x] = Elf()
+                    result[y, x] = Elf(elf_power)
         return result
 
     @staticmethod
@@ -131,7 +131,7 @@ with open(f'2018/15/{board}.txt') as f:
     data = f.read()
 
 lines = data.splitlines()
-state = GameState(lines)
+state: GameState = None
 
 def iter_units():
     seen = set()
@@ -142,15 +142,16 @@ def iter_units():
                 seen.add(unit)
                 yield unit, (y, x)
 
-def find_target(unit: Unit, pos):
-    def get_neighbors(pos):
-        y, x = pos
-        return [
-            (y-1, x),
-            (y, x-1),
-            (y, x+1),
-            (y+1, x)
-        ]
+def get_neighbors(pos):
+    y, x = pos
+    return [
+        (y-1, x),
+        (y, x-1),
+        (y, x+1),
+        (y+1, x)
+    ]
+
+def find_move_target(unit: Unit, pos):
     def create_paths(start_path, steps):
         for s in steps:
             base = list(start_path)
@@ -159,6 +160,7 @@ def find_target(unit: Unit, pos):
     seen = set()
     q = queue.deque([[pos]])
     target_type = 'G' if unit.unit_type == 'E' else 'E'
+    found = []
 
     while len(q) > 0:
         path = q.popleft()
@@ -168,39 +170,65 @@ def find_target(unit: Unit, pos):
             continue
         if len(path) > 1 and not state.can_walk_at(x, y):
             continue
+        if len(found) > 0 and len(path) > len(found[0][1]):
+            continue
         seen.add(last_pos)
         neighbors = get_neighbors(last_pos)
         for n in neighbors:
             found_unit = state.try_get_unit(n)
             if found_unit is not None and found_unit.unit_type == target_type:
-                path.append(n)
-                path = path[1:]
-                return found_unit, path
+                cp = path[1:]
+                cp.append(n)
+                found.append((found_unit, cp))
         for p in create_paths(path, neighbors):
             q.append(p)
 
+    if len(found) > 0:
+        return min(found, key=lambda x: x[1][-1])
+
     return None, None
 
-rnd = 0
-while state.both_unit_types_exist():
-    for unit, pos in iter_units():
-        if not state.both_unit_types_exist():
-            break
-        target, path = find_target(unit, pos)
-        if target is not None:
-            if len(path) > 1:
-                state.move_unit(pos, path[0])
-            if len(path) <= 2:
-                target.hp -= unit.attack
-                if target.hp <= 0:
-                    state.remove_unit(path[-1])
-        # state.print_state()
-    else:
-        rnd += 1
+def find_attack_target(unit, pos):
+    target_type = 'G' if unit.unit_type == 'E' else 'E'
+    min_hp = 300
+    min_unit = None
+    min_pos = None
+    for n in get_neighbors(pos):
+        unit = state.try_get_unit(n)
+        if unit is not None and unit.unit_type == target_type and unit.hp < min_hp:
+            min_hp = unit.hp
+            min_unit = unit
+            min_pos = n
+    return min_unit, min_pos
 
-state.print_state()
-elves_hp = sum(map(lambda x: x.hp, state.elves.values()))
-goblins_hp = sum(map(lambda x: x.hp, state.goblins.values()))
-sum_hp = elves_hp + goblins_hp
+def play(elf_attack_powah=3):
+    global state
+    state = GameState(lines, elf_attack_powah)
+    rnd = 0
+    while state.both_unit_types_exist():
+        for unit, pos in iter_units():
+            if not state.both_unit_types_exist():
+                break
+            curr_pos = pos
+            target, path = find_move_target(unit, pos)
+            if target is not None:
+                if len(path) > 1:
+                    state.move_unit(pos, path[0])
+                    curr_pos = path[0]
+                if len(path) <= 2:
+                    target, target_pos = find_attack_target(unit, curr_pos)
+                    target.hp -= unit.attack
+                    if target.hp <= 0:
+                        state.remove_unit(target_pos)
+            # state.print_state()
+        else:
+            rnd += 1
 
-print(rnd, sum_hp, rnd*sum_hp)
+    state.print_state()
+    elves_hp = sum(map(lambda x: x.hp, state.elves.values()))
+    goblins_hp = sum(map(lambda x: x.hp, state.goblins.values()))
+    sum_hp = elves_hp + goblins_hp
+
+    print(rnd, sum_hp, rnd*sum_hp)
+
+play()
