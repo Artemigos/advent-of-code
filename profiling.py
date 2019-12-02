75f5c5ce2fe69c0041e1dd20afc7a4cd98cec54a
Łukasz Klimek <klinn91@gmail.com>
2019-12-02 17:25:52 +0100

    generalized the profiling script

--- profiling.py
+++ profiling.py
@@ -1,8 +1,18 @@
 import cProfile
+import sys
+
+
+if len(sys.argv) != 2:
+    print('Usage:')
+    print('\tpython3 -m profiling <module>')
+    print()
+    print('Example:')
+    print('\tpython3 -m profiling 2017.16.solution')
+    exit(1)
 
 
 def profile_entry():
-    solution = __import__('2017.16.solution')
+    solution = __import__(sys.argv[1])
     return solution
 
 
