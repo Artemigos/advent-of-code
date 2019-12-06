cab3f583733dab50a97d8fc76f36997552a40f5a
Łukasz Klimek <klinn91@gmail.com>
2019-12-06 22:04:06 +0100

    final solution for 2018/12

--- 2018/12/solution.py
+++ 2018/12/solution.py
@@ -1,3 +1,4 @@
+import common
 import collections as coll
 import queue
 import itertools
@@ -5,71 +6,99 @@ import functools
 import numpy as np
 import re
 
-def read_file(path: str) -> str:
-    with open(path) as f:
-        return f.read()
-
-def read_lines(path: str) -> str:
-    with open(path) as f:
-        return f.read().splitlines()
-
-def extract_words(data: str):
-    return re.findall(r'\w+', data)
-
-def extract_numbers(data: str):
-    return [int(x) for x in re.findall(r'-?\d+', data)]
-
 def parse_line(line: str):
     segments = line.split(' ')
     return (list(segments[0]), segments[2])
 
-data = read_file('2018/12/data.txt').splitlines()
-parsed = [parse_line(x) for x in data]
+def run_simulation(rules_path, initial, generations, label, padding_left = 50, padding_right = 100):
+    data = common.read_file('2018/12/data.txt').splitlines()
+    parsed = [parse_line(x) for x in data]
 
-initial = '..##.#######...##.###...#..#.#.#..#.##.#.##....####..........#..#.######..####.#.#..###.##..##..#..#'
-# initial = '#..#.#..##......###...###'
+    size = len(initial)+padding_left+padding_right
+    i0 = -padding_left
+    state = '.'*padding_left+initial+'.'*padding_right
+    state = list(state)
 
-# generations = 20
-# generations = 50000000000
-generations = 200
-size = len(initial)+150
-i0 = -50
-state = '.'*50+initial+'.'*100
-state = list(state)
+    seen_trim_states = {}
 
-# dict
-st100 = None
-st101 = None
-st102 = None
+    g = 0
+    while g < generations:
+        # calculate new state
+        print(g/generations, end='\r')
+        new_state = '.'*size
+        new_state = list(new_state)
+        for i in range(2, len(state)-2):
+            for rule in parsed:
+                if state[i-2:i+3] == rule[0]:
+                    new_state[i] = rule[1]
+                    break
+            else:
+                new_state[i] = '.'
+        state = new_state
 
-for g in range(generations):
-    if g == 100:
-        st100 = list(state)
-    elif g == 101:
-        st101 = list(state)
-    elif g == 102:
-        st102 = list(state)
-    new_state = '.'*size
-    new_state = list(new_state)
-    for i in range(2, len(state)-2):
-        for rule in parsed:
-            if state[i-2:i+3] == rule[0]:
-                new_state[i] = rule[1]
-                break
+        # diagnostics
+        if g%1000 == 0:
+            for i in range(-1, -7, -1):
+                if state[i] == '#':
+                    print('right end reached!')
+                    break
+            for i in range(0, 6):
+                if state[i] == '#':
+                    print('left end reached!')
+                    break
+
+        # detect travelling loops
+        hashable_state = ''.join(state)
+        trimmed_state = hashable_state.strip('.')
+        start = hashable_state.index('#')
+        if trimmed_state in seen_trim_states:
+            print('travelling loop!', seen_trim_states[trimmed_state], g, start)
+            print(trimmed_state)
+            loop_size = g-seen_trim_states[trimmed_state][0]
+            loop_relocation = start-seen_trim_states[trimmed_state][1]
+            print('loop size', loop_size)
+            print('loop relocation', loop_relocation)
+            print('at generation', g)
+            diff = generations-1-g
+            skipped_loops = diff // loop_size
+            print('skipping', skipped_loops, 'loops')
+            move = skipped_loops * loop_size
+            g += move
+            print('moved to generation', g)
+            print('current offset', i0)
+            i0 += loop_relocation*skipped_loops
+            print('moved to offset', i0)
+            seen_trim_states.clear()
         else:
-            new_state[i] = '.'
-    state = new_state
+            seen_trim_states[trimmed_state] = (g, start)
 
-print()
-mod100 = ['.','.']+st100[:-2]
-mod101 = ['.']+st101[:-1]
-print(mod100)
-print(mod101)
-print(st102)
-print(mod100 == st102)
+        g += 1
 
-acc = 0
-for i in range(len(state)):
-    if state[i] == '#':
-        acc += (i+i0)
-print(acc)
+    # calculate puzzle result
+    acc = 0
+    for i in range(len(state)):
+        if state[i] == '#':
+            acc += (i+i0)
+    print()
+    print(label, acc)
+
+# part 1
+run_simulation(
+    '2018/12/data.txt',
+    '..##.#######...##.###...#..#.#.#..#.##.#.##....####..........#..#.######..####.#.#..###.##..##..#..#',
+    20,
+    'part 1:',
+    1000,
+    1000)
+
+# part 2
+run_simulation(
+    '2018/12/data.txt',
+    '..##.#######...##.###...#..#.#.#..#.##.#.##....####..........#..#.######..####.#.#..###.##..##..#..#',
+    50000000000,
+    'part 2:',
+    1000,
+    2000)
+
+# sample
+# initial = '#..#.#..##......###...###'
