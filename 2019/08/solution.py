61f1a77b716b91804488547bdfff88878ff1570f
Łukasz Klimek <klinn91@gmail.com>
2019-12-10 22:02:26 +0100

    solution for 2019/08

---
+++ 2019/08/solution.py
@@ -0,0 +1,48 @@
+import common
+
+data = common.read_file('2019/08/data.txt')
+width = 25
+height = 6
+
+# data = '0222112222120000'
+# width = 2
+# height = 2
+
+layer_size = width*height
+
+# part 1
+min_zeros = None
+min_result = None
+for layer_offset in range(0, len(data), layer_size):
+    values = {'0': 0, '1': 0, '2': 0}
+    for layer_i in range (layer_offset, layer_offset+layer_size):
+        c = data[layer_i]
+        values[c] += 1
+
+    if min_zeros is None or values['0'] < min_zeros:
+        min_zeros = values['0']
+        min_result = values['1']*values['2']
+
+print('part 1:', min_result)
+
+# part 2
+image = ['2'] * layer_size
+
+for layer_offset in reversed(range(0, len(data), layer_size)):
+    for layer_i in range(layer_offset, layer_offset+layer_size):
+        c = data[layer_i]
+        if c != '2':
+            image[layer_i-layer_offset] = c
+
+print('part 2:')
+
+for y in range(height):
+    for x in range(width):
+        c = '!'
+        image_c = image[width*y+x]
+        if image_c == '0':
+            c = ' '
+        elif image_c == '1':
+            c = '▓'
+        print(c, end='')
+    print()
