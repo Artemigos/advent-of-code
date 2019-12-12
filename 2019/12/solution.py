9d4a35bc68511f8d54e32c8b476a8155d684b17a
Łukasz Klimek <klinn91@gmail.com>
2019-12-12 20:05:33 +0100

    solution for 2019/12 part 2

--- 2019/12/solution.py
+++ 2019/12/solution.py
@@ -1,10 +1,15 @@
+import math
 import common
+from collections import defaultdict
 
 data = common.read_file('2019/12/data.txt').splitlines()
 positions = list(map(tuple, map(common.extract_numbers, data)))
 steps = 1000
 
-# part 1
+def lcm(a, b, c):
+    lcm_ab = abs(a*b)//math.gcd(a, b)
+    return abs(lcm_ab*c)//math.gcd(lcm_ab, c)
+
 curr_pos = list(positions)
 curr_vel = [(0, 0, 0)]*len(curr_pos)
 
@@ -16,8 +21,11 @@ def calc_gravity_center():
     summed = summed[0]/amount, summed[1]/amount, summed[2]/amount
     return summed
 
-gravity_center = 0, 0, 0
-for _ in range(steps):
+step = 0
+seen_states = defaultdict(lambda: dict())
+resolved_loops = dict()
+
+while True:
     new_pos = list(curr_pos)
     for i in range(len(curr_pos)):
         # apply gravity
@@ -38,19 +46,39 @@ for _ in range(steps):
         pos = new_pos[i]
         vel = curr_vel[i]
         new_pos[i] = pos[0]+vel[0], pos[1]+vel[1], pos[2]+vel[2]
+
+    # look for loops
+    for dim in range(3):
+        if dim in resolved_loops:
+            continue
+
+        dim_values = []
+        for i in range(len(curr_pos)):
+            dim_values.append(curr_pos[i][dim])
+            dim_values.append(curr_vel[i][dim])
+        key = tuple(dim_values)
+        if key in seen_states[dim]:
+            resolved_loops[dim] = step-seen_states[dim][key]
+        else:
+            seen_states[dim][key] = step
+
+    if len(resolved_loops) == 3:
+        loop_sizes = list(resolved_loops.values())
+        print('part 2:', lcm(loop_sizes[0], loop_sizes[1], loop_sizes[2]))
+        break
+
+    # update state
     curr_pos = new_pos
-    new_center = calc_gravity_center()
-    if new_center != gravity_center:
-        print('gravity center changed to', new_center)
-        gravity_center = new_center
 
-# calculate energy
-energy = 0
-for i in range(len(curr_pos)):
-    pos = curr_pos[i]
-    vel = curr_vel[i]
-    pot = abs(pos[0])+abs(pos[1])+abs(pos[2])
-    kin = abs(vel[0])+abs(vel[1])+abs(vel[2])
-    energy += pot * kin
+    step += 1
+    if step == steps:
+        # calculate energy
+        energy = 0
+        for i in range(len(curr_pos)):
+            pos = curr_pos[i]
+            vel = curr_vel[i]
+            pot = abs(pos[0])+abs(pos[1])+abs(pos[2])
+            kin = abs(vel[0])+abs(vel[1])+abs(vel[2])
+            energy += pot * kin
 
-print('part 1:', energy)
+        print('part 1:', energy)
