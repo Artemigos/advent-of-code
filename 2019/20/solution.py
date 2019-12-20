9d4e62f09ef93599d21aeedf95ba0e0734bd541d
Łukasz Klimek <lklimek@future-processing.com>
2019-12-20 11:10:21 +0100

    solution for 2019/20 part 2

--- 2019/20/solution.py
+++ 2019/20/solution.py
@@ -14,48 +14,51 @@ for x in range(w):
     for y in range(h):
         c = data[y][x]
         if is_portal_marker(c):
+            is_outer = x == 0 or x == w-2 or y == 0 or y == h-2
             if x+1 < w and is_portal_marker(data[y][x+1]):
                 label = c+data[y][x+1]
                 if x+2 < w and data[y][x+2] == '.':
                     drop_pos = x+2, y
                 else:
                     drop_pos = x-1, y
-                portal_defs[label].append(drop_pos)
+                portal_defs[label].append((drop_pos, is_outer))
             elif y+1 < h and is_portal_marker(data[y+1][x]):
                 label = c+data[y+1][x]
                 if y+2 < h and data[y+2][x] == '.':
                     drop_pos = x, y+2
                 else:
                     drop_pos = x, y-1
-                portal_defs[label].append(drop_pos)
+                portal_defs[label].append((drop_pos, is_outer))
 
 portals = {}
 start = None
 end = None
 for label, locations in portal_defs.items():
     if label == 'AA':
-        start = locations[0]
+        start = locations[0][0][0], locations[0][0][1], 0
     elif label == 'ZZ':
-        end = locations[0]
+        end = locations[0][0][0], locations[0][0][1], 0
     else:
-        portals[locations[0]] = locations[1]
-        portals[locations[1]] = locations[0]
+        portals[locations[0][0]] = locations[1][0][0], locations[1][0][1], locations[0][1]
+        portals[locations[1][0]] = locations[0][0][0], locations[0][0][1], locations[1][1]
 
-for y in range(h):
-    for x in range(w):
-        if (x, y) in portals:
-            print('X', end='')
-        else:
-            print(data[y][x], end='')
-    print()
-print()
+# print board
+# for y in range(h):
+#     for x in range(w):
+#         if (x, y) in portals:
+#             print('X', end='')
+#         else:
+#             print(data[y][x], end='')
+#     print()
+# print()
 
 # part 1
-q = deque([(0, start)])
+q = deque([(0, (start[0], start[1]))])
 seen = set()
+short_end = (end[0], end[1])
 while len(q) > 0:
     depth, pos = q.popleft()
-    if pos == end:
+    if pos == short_end:
         print('part 1:', depth)
         break
 
@@ -72,4 +75,32 @@ while len(q) > 0:
         q.append((depth+1, n))
 
     if pos in portals:
-        q.append((depth+1, portals[pos]))
+        q.append((depth+1, (portals[pos][0], portals[pos][1])))
+
+# part 2
+q = deque([(0, start)])
+seen = set()
+while len(q) > 0:
+    depth, pos = q.popleft()
+    if pos == end:
+        print('part 2:', depth)
+        break
+
+    if pos in seen:
+        continue
+    seen.add(pos)
+
+    x, y, level = pos
+    c = data[y][x]
+    if c != '.':
+        continue
+    if level < 0:
+        continue
+
+    for n in [(x-1, y, level), (x+1, y, level), (x, y-1, level), (x, y+1, level)]:
+        q.append((depth+1, n))
+
+    if (x, y) in portals:
+        new_x, new_y, is_outer = portals[x, y]
+        is_outer_mod = -1 if is_outer else 1
+        q.append((depth+1, (new_x, new_y, level+is_outer_mod)))
