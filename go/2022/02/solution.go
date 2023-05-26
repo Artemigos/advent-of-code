fa64d060d6f8056b82742f5db7f483adc1643559
Łukasz Klimek <klinn91@gmail.com>
2023-05-26 20:52:16 +0200

    finished 2022/02 in Go

--- go/2022/02/solution.go
+++ go/2022/02/solution.go
@@ -23,7 +23,86 @@ func main() {
 		guide = append(guide, Guide{segments[0], segments[1]})
 	}
 
-	fmt.Printf("%v\n", guide)
+	// part 1
+	score := 0
+	for _, g := range guide {
+		score += cScore(g.l, g.r)
+	}
 
-	// TODO: finish
+	fmt.Println(score)
+
+	// part 2
+	score = 0
+	for _, g := range guide {
+		score += cScore(change(g.l, g.r))
+	}
+
+	fmt.Println(score)
+}
+
+func cScore(l string, r string) int {
+	score := 0
+
+	switch r {
+	case "X":
+		score += 1
+		switch l {
+		case "A":
+			score += 3
+		case "C":
+			score += 6
+		}
+	case "Y":
+		score += 2
+		switch l {
+		case "B":
+			score += 3
+		case "A":
+			score += 6
+		}
+	case "Z":
+		score += 3
+		switch l {
+		case "C":
+			score += 3
+		case "B":
+			score += 6
+		}
+	}
+
+	return score
+}
+
+func change(l string, r string) (string, string) {
+	switch r {
+	case "X":
+		switch l {
+		case "A":
+			r = "Z"
+		case "B":
+			r = "X"
+		default:
+			r = "Y"
+		}
+	case "Y":
+		switch l {
+		case "A":
+			r = "X"
+		case "B":
+			r = "Y"
+		default:
+			r = "Z"
+		}
+	default:
+		switch l {
+		case "A":
+			r = "Y"
+		case "B":
+			r = "Z"
+		default:
+			r = "X"
+		}
+	}
+
+	return l, r
 }
