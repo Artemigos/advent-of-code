c2ba8c8ace93d1265c4dd73485a9b4e4d29809f6
Łukasz Klimek <lklimek@future-processing.com>
2023-12-18 14:01:03 +0100

    changed neovim execution mappings

--- nvim-mappings.lua
+++ nvim-mappings.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 local function ends_with(s, ending)
-    return (string.sub(s, string.len(s)+1-string.len(ending)) == ending)
+    return (string.sub(s, string.len(s) + 1 - string.len(ending)) == ending)
 end
 
 function M.run_current(data_file)
@@ -11,17 +11,17 @@ function M.run_current(data_file)
     end
 
     local cwd = vim.fn.getcwd()
-    local rel_path = string.sub(path, string.len(cwd)+2)
+    local rel_path = string.sub(path, string.len(cwd) + 2)
     local module_path = string.gsub(rel_path, '/', '.')
-    module_path = string.sub(module_path, 1, string.len(module_path)-3)
+    module_path = string.sub(module_path, 1, string.len(module_path) - 3)
 
-    local challange_path = string.sub(rel_path, 1, string.len(rel_path)-string.len('solution.py'))
-    local file_path = challange_path..data_file
+    local challange_path = string.sub(rel_path, 1, string.len(rel_path) - string.len('solution.py'))
+    local file_path = challange_path .. data_file
 
-    vim.api.nvim_input(':!pypy3 -m '..module_path..' '..file_path..'<CR>')
+    vim.api.nvim_input(':!pypy3 -m ' .. module_path .. ' ' .. file_path .. '<CR>')
 end
 
-vim.keymap.set('n', '<leader>xx', function() M.run_current('data.txt') end)
-vim.keymap.set('n', '<leader>xs', function() M.run_current('sample.txt') end)
+vim.keymap.set('n', '<leader><CR>', function() M.run_current('data.txt') end)
+vim.keymap.set('n', '<leader><S-Tab>', function() M.run_current('sample.txt') end)
 
 return M
