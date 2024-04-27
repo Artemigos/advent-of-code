0c115d9028863d1af9b0ad09e42d43b3a58dd109
Łukasz Klimek <klinn91@gmail.com>
2024-04-27 23:03:35 +0200

    2015/04 in Go

---
+++ go/2015/04/solution.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"aoc/utils"
+	"crypto/md5"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	data := utils.Read()
+	found5 := false
+	found6 := false
+	five := 0
+	six := 0
+
+	i := 0
+	for {
+		i++
+		hash := md5.New()
+		hash.Write([]byte(data))
+		hash.Write([]byte(strconv.Itoa(i)))
+		sum := hash.Sum(nil)
+
+		if !found5 && sum[0] == 0 && sum[1] == 0 && sum[2]&0xF0 == 0 {
+			found5 = true
+			five = i
+		}
+
+		if !found6 && sum[0] == 0 && sum[1] == 0 && sum[2] == 0 {
+			found6 = true
+			six = i
+		}
+
+		if found5 && found6 {
+			break
+		}
+
+		if i > 10000000 {
+			fmt.Println("went too far")
+			break
+		}
+	}
+
+	fmt.Println(five)
+	fmt.Println(six)
+}
