b03e5ab2dc5f82d1426832c56d67982f2f63f958
Łukasz Klimek <klinn91@gmail.com>
2024-04-27 13:26:53 +0200

    moved python solutions to a subfolder

--- python/profiling.py
+++ python/profiling.py
@@ -10,8 +10,10 @@ if len(sys.argv) != 2:
     print('\tpython3 -m profiling 2017.16.solution')
     exit(1)
 
+module = sys.argv[1]
 
 def profile_entry():
+    module = '2023.20.solution'
     solution = __import__(module)
     return solution
 
