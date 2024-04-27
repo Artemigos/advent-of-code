b3882bc8fac4540fc9ab5f166a32218845d67e95
Łukasz Klimek <klinn91@gmail.com>
2024-04-27 23:09:36 +0200

    cleaned up 2015/04 in Python

--- python/2015/04/solution.py
+++ python/2015/04/solution.py
@@ -4,15 +4,29 @@ data = b'iwrupvqb'
 
 base_hash = hashlib.md5(data)
 
+found5 = False
+found6 = False
+five = 0
+six = 0
+
 i = 0
+
 while True:
     i += 1
     i_str = bytes(str(i), 'utf8')
     cp = base_hash.copy()
     cp.update(i_str)
-    hx = cp.hexdigest()
-    if hx[:5] == '00000':
-        print('5 zeros:', i)
-    if hx[:6] == '000000':
-        print('6 zeros:', i)
+    hx = cp.digest()
+
+    if not found5 and hx[0] == 0 and hx[1] == 0 and hx[2]&0xF0 == 0:
+        five = i
+        found5 = True
+    if not found6 and hx[0] == 0 and hx[1] == 0 and hx[2] == 0:
+        six = i
+        found6 = True
+
+    if found5 and found6:
         break
+
+print(five)
+print(six)
