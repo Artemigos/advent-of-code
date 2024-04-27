55f52f6632f200708d4ec940b5b136a1de36dadc
Łukasz Klimek <klinn91@gmail.com>
2024-04-27 21:31:32 +0200

    allow batch testing specific year

--- test_all.sh
+++ test_all.sh
@@ -2,18 +2,24 @@
 
 set -euo pipefail
 
-MAX_YEAR=${MAX_YEAR:-2023}
+MIN_YEAR=2015
+MAX_YEAR=2023
 
-if [ $# -ne 1 ]; then
-    echo "Usage: ./test_all.sh <LANG>"
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+    echo "Usage: ./test_all.sh <LANG> [YEAR]"
     exit 1
+fi
+
+if [ $# -eq 2 ]; then
+    MIN_YEAR=$2
+    MAX_YEAR=$2
 fi
 
 _lang=$1
 _passed=0
 _total=0
 
-for _year in $(seq 2015 "$MAX_YEAR"); do
+for _year in $(seq "$MIN_YEAR" "$MAX_YEAR"); do
     for _day in $(seq 1 25); do
         _total=$((_total + 1))
         _day=$(printf "%02d" "$_day")
