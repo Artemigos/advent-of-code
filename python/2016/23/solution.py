540a2a05d84bf95fefa0558ffce466b993662bbe
Łukasz Klimek <klinn91@gmail.com>
2024-05-03 23:55:15 +0200

    solution for python/2016/23 using reverse-engineered version of input

---
+++ python/2016/23/solution.py
@@ -0,0 +1,4 @@
+from .transcript2 import f
+
+f(7)
+f(12)
--- python/2016/23/transcript2.py
+++ python/2016/23/transcript2.py
@@ -1,11 +1,11 @@
-a = 12
-b = a-1
+def f(a: int):
+    b = a-1
 
-while True:
-    a *= b
-    b -= 1
-    if b == 1:
-        break
+    while True:
+        a *= b
+        b -= 1
+        if b == 1:
+            break
 
-a += 90*90
-print(a)
+    a += 90*90
+    print(a)
