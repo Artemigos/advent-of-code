2e5db422f4c4673991e20022851057ce5286fdf5
Łukasz Klimek <klinn91@gmail.com>
2024-05-03 19:59:27 +0200

    updated python/2015/17 to read data from file

--- python/2015/17/solution.py
+++ python/2015/17/solution.py
@@ -1,11 +1,7 @@
 import itertools
+import common
 
-data = [
-    43, 3, 4, 10, 21,
-    44, 4, 6, 47, 41,
-    34, 17, 17, 44, 36,
-    31, 46, 9, 27, 38
-]
+data = [int(x) for x in common.read_file().splitlines()]
 
 target = 150
 
