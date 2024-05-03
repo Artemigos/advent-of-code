c029c6c31bcd2f4b594bd86180cd32b76442116d
Łukasz Klimek <klinn91@gmail.com>
2024-05-03 21:29:43 +0200

    updated python/2015/24 to read data from file

--- python/2015/24/solution.py
+++ python/2015/24/solution.py
@@ -1,14 +1,8 @@
 import itertools
 import functools
+import common
 
-weights = [
-    1, 3, 5, 11, 13,
-    17, 19, 23, 29, 31,
-    41, 43, 47, 53, 59,
-    61, 67, 71, 73, 79,
-    83, 89, 97, 101, 103,
-    107, 109, 113
-]
+weights = [int(x) for x in common.read_file().splitlines()]
 
 def product(iterable):
     return functools.reduce(lambda acc, x: acc*x, iterable, 1)
