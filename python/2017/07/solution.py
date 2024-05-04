27317223d4480f1e0e50eae26cf4df4b8bd0efa6
Łukasz Klimek <klinn91@gmail.com>
2024-05-04 21:29:11 +0200

    part 2 for python/2017/07

--- python/2017/07/solution.py
+++ python/2017/07/solution.py
@@ -31,15 +31,58 @@ print(current)
 # part 2
 nodes = list(map(parse_line_with_weight, lines))
 weights = {node: weight for node, weight in nodes}
+rev_rels = {}
 for node, weight in nodes:
     curr = node
     while curr in relations.keys():
-        curr = relations[curr]
+        parent = relations[curr]
+        if parent not in rev_rels:
+            rev_rels[parent] = set()
+        rev_rels[parent].add(curr)
+        curr = parent
         weights[curr] += weight
 
-s_weights = sorted(weights, key=lambda x: weights[x])
+curr = current
+while True:
+    ch = [(x, weights[x]) for x in rev_rels[curr]]
+    counts = {}
+    for n, w in ch:
+        if w in counts:
+            counts[w] += 1
+        else:
+            counts[w] = 1
+    ch_incorrect = len(counts) > 1
+    if not ch_incorrect:
+        break
+    if len(ch) == 2:
+        assert False, 'inconclusive who is incorrect'
+    for k in counts:
+        if counts[k] == 1:
+            incorrect_w = k
+            break
+    else:
+        assert False, 'failed child weight has more occurences?'
+    for n, w in ch:
+        if w == incorrect_w:
+            incorrect_n = n
+            break
+    else:
+        assert False, 'child with incorrect weight not found'
+    curr = incorrect_n
 
-# for k in s_weights:
-#     print(k, weights[k])
-# NOTE: with this data I did manual analysis
-# TODO: finish implementation
+siblings = list(rev_rels[relations[curr]])
+siblings.remove(curr)
+correct_sibling = siblings[0]
+
+for n, w in nodes:
+    if n == curr:
+        incorrect_sibling_w = w
+        break
+else:
+    assert False, 'incorrect sibling not found'
+
+incorrect_total_w = weights[curr]
+correct_total_w = weights[correct_sibling]
+
+diff = correct_total_w - incorrect_total_w
+print(incorrect_sibling_w + diff)
