53c909d2fd3b26efaee02b46c4ada0e3ee8f2f47
Łukasz Klimek <klinn91@gmail.com>
2024-12-03 19:38:45 +0100

    adapted result fetching script to wayland

--- fetch_results.sh
+++ fetch_results.sh
@@ -4,19 +4,10 @@ set -euo pipefail
 
 _year=${1?year required}
 
-for _day in $(seq 1 24); do
+for _day in $(seq 1 25); do
     _day=$(printf "%02d" "$_day")
     _file=data/${_year}/${_day}/result.txt
-    echo "Waiting for day $_day part 1..."
-    clipnotify && xclip -o >"$_file"
-    echo >>"$_file"
-    echo "Waiting for day $_day part 2..."
-    clipnotify && xclip -o >>"$_file"
-    echo >>"$_file"
+    echo "Waiting for day $_day... [ENTER]"
+    read
+    wl-paste >"$_file"
 done
-
-_day=25
-_file=data/${_year}/${_day}/result.txt
-echo "Waiting for day $_day part 1..."
-clipnotify && xclip -o >"$_file"
-echo >>"$_file"
