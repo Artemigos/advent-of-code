60e29f7d29726a20b046b1772f7ebaea13786805
Łukasz Klimek <lklimek@future-processing.com>
2024-12-03 13:10:55 +0100

    improved nvim mappings to work with any language

--- nvim-mappings.lua
+++ nvim-mappings.lua
@@ -1,27 +1,23 @@
 local M = {}
 
-local function ends_with(s, ending)
-    return (string.sub(s, string.len(s) + 1 - string.len(ending)) == ending)
-end
-
-function M.run_current(data_file)
+function M.run_current()
     local path = vim.api.nvim_buf_get_name(0)
-    if not ends_with(path, 'solution.py') then
-        return
-    end
-
     local cwd = vim.fn.getcwd()
     local rel_path = string.sub(path, string.len(cwd) + 2)
-    local module_path = string.gsub(rel_path, '/', '.')
-    module_path = string.sub(module_path, 1, string.len(module_path) - 3)
-
-    local challange_path = string.sub(rel_path, 1, string.len(rel_path) - string.len('solution.py'))
-    local file_path = challange_path .. data_file
-
-    vim.api.nvim_input(':!pypy3 -m ' .. module_path .. ' ' .. file_path .. '<CR>')
+    local lang, year, day = string.match(rel_path, '^(%w+)/(%d+)/(%d+)/.+$')
+    -- can't use vim.cmd - it prints the output over the command (IMO incorrectly)
+    -- can't use vim.system - it doesn't print the output
+    vim.api.nvim_input(':!./run.sh run ' .. lang .. ' ' .. year .. ' ' .. day .. '<CR>')
 end
 
-vim.keymap.set('n', '<leader><CR>', function() M.run_current('data.txt') end)
-vim.keymap.set('n', '<leader><S-Tab>', function() M.run_current('sample.txt') end)
+function M.setup()
+    local run_map = '<leader><CR>'
+    if vim.fn.maparg(run_map, 'n') ~= '' then
+        vim.keymap.del('n', run_map)
+    end
+    vim.keymap.set('n', run_map, M.run_current)
+end
+
+M.setup()
 
 return M
