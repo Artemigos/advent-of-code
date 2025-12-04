fc96b5fcd70ebcf792c67850a40bfa0b49094a14
Łukasz Klimek <klinn91@gmail.com>
2025-12-04 21:31:43 +0100

    converted sample in zig 2025/01 to a test

--- zig/2025/01/main.zig
+++ zig/2025/01/main.zig
@@ -1,63 +1,76 @@
 const std = @import("std");
 
-const sample =
-    \\L68
-    \\L30
-    \\R48
-    \\L5
-    \\R60
-    \\L55
-    \\L1
-    \\L99
-    \\R14
-    \\L82
-;
-
 pub fn main() !void {
     const file = try readFileFromArg();
     defer file.close();
     var file_buffer: [4096]u8 = undefined;
-    var r = file.reader(&file_buffer);
-    var reader = &r.interface;
+    var file_reader = file.reader(&file_buffer);
+    const result = try processBuf(&file_reader.interface);
+    try printStdOutUnsafe("{}\n{}\n", .{ result.part1, result.part2 });
+}
 
-    // read sample
-    // var reader = std.io.Reader.fixed(sample);
+const Result = struct {
+    part1: u64,
+    part2: u64,
+};
 
-    // process line by line
-    var result_part1: usize = 0;
-    var result_part2: usize = 0;
+fn processBuf(reader: *std.io.Reader) !Result {
+    var part1: usize = 0;
+    var part2: usize = 0;
     var state: isize = 50;
     while (try reader.takeDelimiter('\n')) |line| {
         const dir = line[0];
         const val = try std.fmt.parseInt(isize, line[1..], 10);
         if (dir == 'L') {
             if (state == 0) {
-                result_part2 -= 1;
+                part2 -= 1;
             }
             state -= val;
             while (state < 0) {
                 state += 100;
-                result_part2 += 1;
+                part2 += 1;
             }
             if (state == 0) {
-                result_part2 += 1;
+                part2 += 1;
             }
         } else if (dir == 'R') {
             state += val;
             while (state > 99) {
                 state -= 100;
-                result_part2 += 1;
+                part2 += 1;
             }
         } else {
             return error.UnexpectedDirection;
         }
 
         if (state == 0) {
-            result_part1 += 1;
+            part1 += 1;
         }
     }
+    return .{
+        .part1 = part1,
+        .part2 = part2,
+    };
+}
 
-    try printStdOutUnsafe("{}\n{}\n", .{ result_part1, result_part2 });
+test "sample" {
+    const sample =
+        \\L68
+        \\L30
+        \\R48
+        \\L5
+        \\R60
+        \\L55
+        \\L1
+        \\L99
+        \\R14
+        \\L82
+        \\
+    ;
+    var reader = std.io.Reader.fixed(sample);
+    const result = try processBuf(&reader);
+    try std.testing.expectEqual(3, result.part1);
+    try std.testing.expectEqual(6, result.part2);
 }
 
 fn readFileFromArg() !std.fs.File {
