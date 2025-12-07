9cad1367775d4c5895da1db34925f65a8563b4bc
Łukasz Klimek <klinn91@gmail.com>
2025-12-07 17:07:53 +0100

    zig 2025/07

---
+++ zig/2025/07/main.zig
@@ -0,0 +1,197 @@
+const std = @import("std");
+
+const buf_cap: usize = 30000;
+const lines_cap: usize = 200;
+
+pub fn main() !void {
+    const file = try readFileFromArg();
+    defer file.close();
+    var buf: [4096]u8 = undefined;
+    var reader = file.reader(&buf);
+    const result = try processBuf(&reader.interface);
+    try printStdOutUnsafe("{}\n{}\n", .{ result.part1, result.part2 });
+}
+
+const Result = struct {
+    part1: u64,
+    part2: u64,
+};
+
+fn processBuf(reader: *std.io.Reader) !Result {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var data_buf: [buf_cap]u8 = undefined;
+    var lines_buf: [lines_cap][]u8 = undefined;
+    const buf = try readToBuffer(&data_buf, reader);
+    const lines = try findLines(buf, &lines_buf);
+
+    const s_x = std.mem.indexOfScalar(u8, lines[0], 'S').?;
+    const s = Point{
+        .x = s_x,
+        .y = 0,
+    };
+
+    const fist_splitter = findSplitterBelow(lines, s).?;
+    var finder = MemoFindPaths.init(allocator);
+    defer finder.deinit();
+    const paths = try finder.call(lines, fist_splitter);
+
+    return .{
+        .part1 = finder.cache.count(),
+        .part2 = paths,
+    };
+}
+
+test "sample" {
+    const sample =
+        \\.......S.......
+        \\...............
+        \\.......^.......
+        \\...............
+        \\......^.^......
+        \\...............
+        \\.....^.^.^.....
+        \\...............
+        \\....^.^...^....
+        \\...............
+        \\...^.^...^.^...
+        \\...............
+        \\..^...^.....^..
+        \\...............
+        \\.^.^.^.^.^...^.
+        \\...............
+        \\
+    ;
+    var reader = std.io.Reader.fixed(sample);
+    const result = try processBuf(&reader);
+    try std.testing.expectEqual(21, result.part1);
+    try std.testing.expectEqual(40, result.part2);
+}
+
+const MemoFindPaths = struct {
+    cache: std.AutoHashMap(Point, usize),
+
+    fn init(allocator: std.mem.Allocator) MemoFindPaths {
+        return .{
+            .cache = std.AutoHashMap(Point, usize).init(allocator),
+        };
+    }
+
+    fn deinit(self: *MemoFindPaths) void {
+        self.cache.deinit();
+    }
+
+    fn call(
+        self: *MemoFindPaths,
+        lines: [][]u8,
+        splitter: Point,
+    ) std.mem.Allocator.Error!usize {
+        if (self.cache.get(splitter)) |ret| {
+            return ret;
+        }
+        const ret = try self._impl(lines, splitter);
+        try self.cache.put(splitter, ret);
+        return ret;
+    }
+
+    fn _impl(
+        self: *MemoFindPaths,
+        lines: [][]u8,
+        splitter: Point,
+    ) !usize {
+        var result: usize = 0;
+        if (splitter.x > 0) {
+            if (findSplitterBelow(lines, .{
+                .x = splitter.x - 1,
+                .y = splitter.y,
+            })) |next| {
+                result += try self.call(
+                    lines,
+                    next,
+                );
+            } else {
+                result += 1;
+            }
+        }
+        if (splitter.x < lines[0].len - 1) {
+            if (findSplitterBelow(lines, .{
+                .x = splitter.x + 1,
+                .y = splitter.y,
+            })) |next| {
+                result += try self.call(
+                    lines,
+                    next,
+                );
+            } else {
+                result += 1;
+            }
+        }
+        return result;
+    }
+};
+
+const Point = struct {
+    x: usize,
+    y: usize,
+};
+
+fn findSplitterBelow(lines: [][]u8, start: Point) ?Point {
+    var y: usize = start.y;
+    while (y < lines.len) : (y += 1) {
+        if (lines[y][start.x] == '^') {
+            return .{
+                .x = start.x,
+                .y = y,
+            };
+        }
+    }
+    return null;
+}
+
+fn readFileFromArg() !std.fs.File {
+    var args = std.process.args();
+    _ = args.next();
+    const path = args.next();
+    if (path == null) {
+        return error.InvalidNumberOfArguments;
+    }
+    return std.fs.cwd().openFile(path.?, .{});
+}
+
+fn printStdOutUnsafe(comptime fmt: []const u8, args: anytype) !void {
+    var buf: [64]u8 = undefined;
+    var writer = std.fs.File.stdout().writer(&buf);
+    try writer.interface.print(fmt, args);
+    try writer.interface.flush();
+}
+
+fn readToBuffer(buffer: []u8, reader: *std.io.Reader) ![]u8 {
+    const len = try reader.readSliceShort(buffer);
+    if (len == buffer.len) {
+        return error.BufferTooSmall;
+    }
+    return buffer[0..len];
+}
+
+fn findLines(buf: []u8, lines_buf: [][]u8) ![][]u8 {
+    var lines = std.ArrayList([]u8){
+        .items = lines_buf[0..0],
+        .capacity = lines_buf.len,
+    };
+
+    var offset: usize = 0;
+    while (offset < buf.len) {
+        const next_newline = std.mem.indexOfScalarPos(u8, buf, offset, '\n');
+        if (next_newline) |val| {
+            try lines.appendBounded(buf[offset..val]);
+            offset = val + 1;
+        } else {
+            try lines.appendBounded(buf[offset..buf.len]);
+            break;
+        }
+    }
+
+    return lines.items;
+}
